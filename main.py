93c6802468ce9e0ac5bd27394719e0b972da5a36
Neo <pytandgit@gmail.com>
2025-07-30 17:15:14 +0300

    Добавлены слова Широта и Долгота

--- main.py
+++ main.py
@@ -7,14 +7,14 @@ def get_coordinates(city, key):
         if results:
             lat = round(results[0]['geometry']['lat'], 2)
             lon = round(results[0]['geometry']['lng'], 2)
-            return lat, lon
+            return f'Широта: {lat}, Долгота: {lon}'
         else:
             return 'Город не найден'
     except Exception as e:
         return f'Возникла ошибка: {e}'
 
 key = '5cf6dcaf3ada4335bd40f2aeb9962d85'
-city = 'Moscow'
+city = 'Эквадор'
 coordinates = get_coordinates(city, key)
 
 print(f'Координаты города {city}: {coordinates}')
